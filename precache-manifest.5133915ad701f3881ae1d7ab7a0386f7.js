4e26cd33c30a9abee43e96004b490581ef6366e4
MOJEED KUSIMO <mkusimo90@gmail.com>
2020-08-05 04:11:10 +0100

    Updates

---
+++ precache-manifest.5133915ad701f3881ae1d7ab7a0386f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8174525a37857b5e7d2c706a3c414f4c",
+    "url": "/faydroyal/index.html"
+  },
+  {
+    "revision": "da04a47d529a794c2a4f",
+    "url": "/faydroyal/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "d1ad73ef7d5d8eb0961d",
+    "url": "/faydroyal/static/js/2.8aac0e6c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/faydroyal/static/js/2.8aac0e6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da04a47d529a794c2a4f",
+    "url": "/faydroyal/static/js/main.9038906a.chunk.js"
+  },
+  {
+    "revision": "99acdb6726b093296e60",
+    "url": "/faydroyal/static/js/runtime-main.6dc763dc.js"
+  }
+]);
